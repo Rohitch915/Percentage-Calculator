b51b4eb5a9e20e72a4c1893f5306cefd7d623f65
CH Srinivas <srinivas.ch915@gmail.com>
2023-12-16 10:12:51 +0530

    js source updated.

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
-let number=document.querySelectorAll('.number');
-let button=document.querySelector('.button');
-let result=document.querySelector('.result');
-button.addEventListener('click',function(e) {
-   e.preventDefault();
-   let sum=0;
-   if(Number(number[0].value)>=0 && Number(number[1].value)>=0 && Number(number[2].value)>=0 && Number(number[3].value)>=0)
-   {
-   for(let i=0;i<4;i++)
-   {
-      sum+=Number(number[i].value);
-   }
-  
-   result.innerHTML="You have got " +sum+" marks out of 400 and your percentage is "+Math.round((sum/400)*100)+"%";
-  }
-});
-function cal(event)
-{
+let number = document.querySelectorAll('.number');
+let button = document.querySelector('.button');
+let result1 = document.querySelector('.result');
+
+function calc(event) {
    event.preventDefault();
-   let sum=0;
-   if(Number(number[0].value)>=0 && Number(number[1].value)>=0 && Number(number[2].value)>=0 && Number(number[3].value)>=0)
-   {
-   for(let i=0;i<4;i++)
-   {
-      sum+=Number(number[i].value);
+   let sum = 0;
+   if (Number(number[0].value) >= 0 && Number(number[1].value) >= 0 && Number(number[2].value) >= 0 && Number(number[3].value) >= 0) {
+      for (let i = 0; i < 4; i++) {
+         sum += Number(number[i].value);
+      }
+
+      result1.innerHTML = "You have got " + sum + " marks out of 400 and your percentage is " + Math.round((sum / 400) * 100) + "%";
    }
-  
-   result.innerHTML="You have got " +sum+" marks out of 400 and your percentage is "+Math.round((sum/400)*100)+"%";
-  }
-  
-  
-}
-function result(sum,event)
-{
-   result.innerHTML="You have got " +sum+" marks out of 400 and your percentage is "+Math.round((sum/400)*100)+"%";
-   
+
+
 }
